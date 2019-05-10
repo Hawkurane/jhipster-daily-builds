baa8466731ed37ccdf62835e583867f27f87c76d
Jérôme PHAM <hawkurane@gmail.com>
2019-05-10 15:56:29 +0200

    added tests for back ends

---
+++ test-integration/jdl-scripts/21-tests-backend.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+source $(dirname $0)/00-init-env.sh
+
+#-------------------------------------------------------------------------------
+# Display environment information like JDK version
+#-------------------------------------------------------------------------------
+cd "$JHI_FOLDER_APP"
+for local_folder in $(ls "$JHI_FOLDER_APP"); do
+    if [ -d "$local_folder" ];
+    then
+        cd $local_folder
+        if [ -f "mvnw" ]; then
+            ./mvnw enforcer:display-info
+        elif [ -f "gradlew" ]; then
+            ./gradlew -v
+        fi
+
+        #-------------------------------------------------------------------------------
+        # Check Javadoc generation
+        #-------------------------------------------------------------------------------
+        if [ -f "mvnw" ]; then
+            ./mvnw javadoc:javadoc
+        elif [ -f "gradlew" ]; then
+            ./gradlew javadoc
+        fi
+
+        #-------------------------------------------------------------------------------
+        # Launch UAA tests
+        #-------------------------------------------------------------------------------
+        if [[ "$JHI_APP" == *"uaa"* ]]; then
+            cd "$JHI_FOLDER_UAA"
+            ./mvnw verify
+        fi
+    fi
+done
+for local_folder in $(ls "$JHI_FOLDER_APP"); do
+    if [ -d "$local_folder" ];
+    then
+        #-------------------------------------------------------------------------------
+        # Launch tests
+        #-------------------------------------------------------------------------------
+        cd "$local_folder"
+        if [ -f "mvnw" ]; then
+            ./mvnw verify \
+                -Dlogging.level.ROOT=OFF \
+                -Dlogging.level.org.zalando=OFF \
+                -Dlogging.level.io.github.jhipster=OFF \
+                -Dlogging.level.io.github.jhipster.sample=OFF \
+                -Dlogging.level.io.github.jhipster.travis=OFF \
+                -Dlogging.level.org.springframework=OFF \
+                -Dlogging.level.org.springframework.web=OFF \
+                -Dlogging.level.org.springframework.security=OFF
+
+        elif [ -f "gradlew" ]; then
+            ./gradlew test integrationTest \
+                -Dlogging.level.ROOT=OFF \
+                -Dlogging.level.org.zalando=OFF \
+                -Dlogging.level.io.github.jhipster=OFF \
+                -Dlogging.level.io.github.jhipster.sample=OFF \
+                -Dlogging.level.io.github.jhipster.travis=OFF \
+                -Dlogging.level.org.springframework=OFF \
+                -Dlogging.level.org.springframework.web=OFF \
+                -Dlogging.level.org.springframework.security=OFF
+        fi
+    fi
+done
