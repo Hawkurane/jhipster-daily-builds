26ea423437e69d115703a14f2e75546aa14c2efd
Jérôme PHAM <hawkurane@gmail.com>
2019-05-20 11:47:17 +0200

    chmod on docker compose script

--- test-integration/jdl-scripts/24-deploy-with-docker-compose.sh
+++ test-integration/jdl-scripts/24-deploy-with-docker-compose.sh
@@ -6,6 +6,7 @@ source $(dirname $0)/00-init-env.sh
 if [ -d "$JHI_FOLDER_APP"/docker-compose ]; then
     cd "$JHI_FOLDER_APP"/docker-compose
     if [ -a docker-compose.yml ]; then
-        docker-compose -f docker-compose.ymlup -d
+        chmod +x docker-compose.yml
+        docker-compose -f docker-compose.yml up -d
     fi
 fi
