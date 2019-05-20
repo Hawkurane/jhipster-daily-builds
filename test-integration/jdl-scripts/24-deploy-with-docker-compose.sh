fed221ee85cc23dbd6592a065955db380454cefb
Jérôme PHAM <hawkurane@gmail.com>
2019-05-20 14:03:08 +0200

    a

--- test-integration/jdl-scripts/24-deploy-with-docker-compose.sh
+++ test-integration/jdl-scripts/24-deploy-with-docker-compose.sh
@@ -6,6 +6,8 @@ source $(dirname $0)/00-init-env.sh
 if [ -d "$JHI_FOLDER_APP"/docker-compose ]; then
     cd "$JHI_FOLDER_APP"/docker-compose
     if [ -a docker-compose.yml ]; then
-        docker-compose -f docker-compose.yml up -d
+        docker-compose up -d
     fi
 fi
+
+docker ps -a
