576a1716ad3d98bcf87e0a4a7ac76aba6ae05e99
Jérôme PHAM <hawkurane@gmail.com>
2019-05-20 11:06:43 +0200

    fix attempt in 24th script

--- test-integration/jdl-scripts/24-deploy-to-docker-compose.sh
+++ test-integration/jdl-scripts/24-deploy-to-docker-compose.sh
@@ -3,7 +3,9 @@
 set -e
 source $(dirname $0)/00-init-env.sh
 
-cd "$JHI_FOLDER_APP/docker-compose"
-if [ -a docker-compose.yml ]; then
-    docker-compose up -d
+if [ -d "$JHI_FOLDER_APP"/docker-compose ]; then
+    cd "$JHI_FOLDER_APP"/docker-compose
+    if [ -a docker-compose.yml ]; then
+        docker-compose up -d
+    fi
 fi
