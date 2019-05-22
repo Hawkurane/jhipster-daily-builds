832a67ff98bedbce6ef78783fa2174b9f4d589b6
Jérôme PHAM <hawkurane@gmail.com>
2019-05-22 11:15:51 +0200

    start kub impl

---
+++ test-integration/jdl-scripts/26-install-kind.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+source $(dirname $0)/00-init-env.sh
+
+
+#-------------------------------------------------------------------------------
+# Installing kind
+#-------------------------------------------------------------------------------
+curl -Lo ./kind-linux-amd64 https://github.com/kubernetes-sigs/kind/releases/download/v0.3.0/kind-linux-amd64
+chmod +x ./kind-linux-amd64
+mv ./kind-linux-amd64 $(Build.BinariesDirectory)/kind
+
+kind --version
