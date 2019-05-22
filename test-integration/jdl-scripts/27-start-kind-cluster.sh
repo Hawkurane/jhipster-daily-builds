62799f97e156a53001b78c6b73b8afbaf6cc1dc3
Jérôme PHAM <hawkurane@gmail.com>
2019-05-22 18:06:34 +0200

    test

--- test-integration/jdl-scripts/27-start-kind-cluster.sh
+++ test-integration/jdl-scripts/27-start-kind-cluster.sh
@@ -14,4 +14,17 @@ source $(dirname $0)/00-init-env.sh
 #-------------------------------------------------------------------------------
 kind create cluster
 export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
+
 kubectl cluster-info
+
+#-------------------------------------------------------------------------------
+# Start kubernetes deployment
+#-------------------------------------------------------------------------------
+if [ -d "$JHI_FOLDER_APP"/kubernetes ]; then
+    cd "$JHI_FOLDER_APP"/kubernetes
+    if [ -a kubectl-apply.sh ]; then
+        ./kubectl-apply.sh
+    fi
+fi
+
+kubectl get service -n jhipster
