fccaba1bcaf010e1c1cb892a7b528298f5b08cee
Jérôme PHAM <hawkurane@gmail.com>
2019-05-23 10:07:37 +0200

    added e2e tests for kub

--- test-integration/jdl-scripts/28-deploy-with-kubernetes.sh
+++ test-integration/jdl-scripts/28-deploy-with-kubernetes.sh
@@ -4,6 +4,12 @@ set -e
 source $(dirname $0)/00-init-env.sh
 
 #-------------------------------------------------------------------------------
+# Connecting to cluster
+#-------------------------------------------------------------------------------
+export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
+kubectl cluster-info
+
+#-------------------------------------------------------------------------------
 # Start kubernetes deployment
 #-------------------------------------------------------------------------------
 if [ -d "$JHI_FOLDER_APP"/kubernetes ]; then
