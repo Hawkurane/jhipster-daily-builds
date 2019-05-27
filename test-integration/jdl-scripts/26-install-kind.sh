533d17fe0a73f3f4a7b786a8ca618578d015978b
Jérôme PHAM <hawkurane@gmail.com>
2019-05-27 11:22:37 +0200

    shaolans

--- test-integration/jdl-scripts/26-install-kind.sh
+++ test-integration/jdl-scripts/26-install-kind.sh
@@ -5,9 +5,28 @@ source $(dirname $0)/00-init-env.sh
 
 
 #-------------------------------------------------------------------------------
-# Installing kind
+# Updating
 #-------------------------------------------------------------------------------
-curl -Lo ./kind-linux-amd64 https://github.com/kubernetes-sigs/kind/releases/download/v0.3.0/kind-linux-amd64
-chmod +x ./kind-linux-amd64
-sudo mv ./kind-linux-amd64 /usr/local/bin/kind
-kind --version
+
+sudo apt-get purge docker-engine
+sudo apt-get autoremove --purge docker-engine
+sudo rm -rf /var/lib/docker
+curl https://releases.rancher.com/install-docker/17.03.sh | sh
+
+
+#-------------------------------------------------------------------------------
+# Installing minikube
+#-------------------------------------------------------------------------------
+
+# Adapted from: https://github.com/LiliC/travis-minikube/blob/minikube-26-kube-1.10/.travis.yml
+
+export CHANGE_MINIKUBE_NONE_USER=true
+
+echo "--> Downloading minikube"
+# Make root mounted as rshared to fix kube-dns issues.
+sudo mount --make-rshared /
+# Download kubectl, which is a requirement for using minikube.
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.12.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+# Download minikube.
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
