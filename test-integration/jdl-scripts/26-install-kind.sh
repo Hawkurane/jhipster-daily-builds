14303e678f50fc094f6ec042712958a238be750c
Jérôme PHAM <hawkurane@gmail.com>
2019-05-27 09:45:05 +0200

    installing virtualbox

--- test-integration/jdl-scripts/26-install-kind.sh
+++ test-integration/jdl-scripts/26-install-kind.sh
@@ -26,3 +26,12 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
 chmod +x minikube
 sudo cp minikube /usr/local/bin && rm minikube
+
+#-------------------------------------------------------------------------------
+# Installing Virtual Box
+#-------------------------------------------------------------------------------
+wget -q -O- http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
+echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list 
+sudo apt-get update
+sudo apt-get install virtualbox-5.2
+sudo usermod -G vboxusers -a $USER
